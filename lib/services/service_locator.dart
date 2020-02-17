1c0288354244a975685cb5e1e8918891a70571a1
AAYUSH KHURANA <31439631+Aayushk333@users.noreply.github.com>
2020-02-18 03:03:45 +0530

    Add files via upload

---
+++ lib/services/service_locator.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_integration/services/calls_and_messages_service.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt locator = GetIt();
+
+void setupLocator() {
+  locator.registerSingleton(CallsAndMessagesService());
+}
+
